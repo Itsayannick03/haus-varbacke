97f68f12f51f0949ffb899ad388a32c880a899da
Itsayannick03 <yannickwinkler3@gmail.com>
2025-09-16 17:46:18 +0200

    Added unfinished language and booking button

--- frontend/app/header.tsx
+++ frontend/app/header.tsx
@@ -6,25 +6,34 @@ export function Header()
     return(
         <div className='mainHeader' >
             <img className='img' src="moose.png" alt=""  />
-            <div className='textBox'>
+            <div className='centerGroup'>
                 <div>
-                    <a href="/">Home</a>
-                </div>
-                <div>
-                    <a href="/">Prices</a>
+                     <a className='bookingBtn' href="/">Language</a>
                 </div>
 
-                <div>
-                    <a href="/">Booking</a>
+                <div className='textBox'>
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <div>
+                        <a href="/">Prices</a>
+                    </div>
+
+                    
+                    
+                    <div>
+                        <a href="/">About us</a>
+                    </div>
+                    <div>
+                        <a href="/">Contact</a>
+                    </div>
                 </div>
                 
                 <div>
-                    <a href="/">About us</a>
-                </div>
-                <div>
-                    <a href="/">Contact</a>
+                    <a className='bookingBtn' href="/">Booking</a>
                 </div>
             </div>
+            
 
             <div className='rightSide'>
 

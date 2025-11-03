fc05782b9e42f459eaba4adf81278f7484db0838
Itsayannick03 <yannickwinkler3@gmail.com>
2025-11-03 23:18:28 +0100

    Getting started

--- frontend/app/welcome/welcome.tsx
+++ frontend/app/welcome/welcome.tsx
@@ -1,49 +1,40 @@
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
+import "../styles/welcome.css"
+import "aos/dist/aos.css";
+
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
-          </div>
-        </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
-            </p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
+  <div className="text-stroke">
+    <div className="relative ">
+      
+      <div >
+        <img  src="stad.jpg" alt=""/>  
+        <div data-aos="fade-zoom-in" data-aos-duration="1500" data-aos-easing="linear"  className="absolute inset-0 flex flex-col space-y-4 justify-center items-center text-white text-7xl font-bold tracking-wide drop-shadow text-shadow ">
+              
+          <p >Herzlich Willkommen in Haus Vårbacke!</p>
+          <p >oder wie man auf schwedisch sagt</p>
+          <p className="italic">"Välkomna till hus vårbacke!"</p>
         </div>
+        
       </div>
-    </main>
-  );
+    </div>
+
+    <div className="relative">
+      <img data-aos="fade-zoom-in" data-aos-duration="2000" data-aos-offset="30" data-aos-once="true" data-aos-easing="linear" src="hus.JPG" alt="" />
+      <div className="absolute inset-0 flex flex-col space-y-4 justify-center items-center text-white text-7xl font-bold tracking-wide drop-shadow text-shadow ">
+        <p>Ein Zuhause auf dem Frühlingshügel.</p>
+        <p>Ruhe, Natur und Raum für die ganze Familie.</p>
+        <button className="btn">
+  Mehr über das Haus
+</button>
+
+      </div>
+    </div>
+  </div>
+  
+  )
 }
 
 const resources = [

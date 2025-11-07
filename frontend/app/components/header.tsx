8ea442f2734fe7eccc2014edb81c815f46ecbcc4
Itsayannick03 <yannickwinkler3@gmail.com>
2025-11-07 21:00:20 +0100

    Tweaks in header

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -6,26 +6,26 @@ export function Header()
     return(
     <header className="font-sans text-white fixed top-0 left-0 w-full h-20 bg-white/30 backdrop-blur-lg z-50 flex  items-center justify-center">
         <div className="">
-            <p  className="font-serif absolute text-xl top-3 left-5 w-32 h-auto object-contain">Ferienhaus Vårbacke</p>
+            <p  className="text-gray-800 font-serif absolute text-3xl top-1 left-5 w-32 h-auto object-contain">Ferienhaus Vårbacke</p>
         </div>
     
         
-        <nav className="font-serif text-xl text-gray-800 flex flex-col justify-baseline  items-center text-lg">
+        <nav className=" font-serif text-xl text-gray-800 flex flex-col justify-baseline  items-center text-lg">
             
             <div className="flex-row space-x-10">
-                <a href="#contact" className="hover:text-gray-600">Home</a>
-                <a href="#about" className="hover:text-gray-600">Über das Haus</a>
+                <a href="/" className="hover:text-gray-600">Home</a>
+                <a href="#uberHaus" className="hover:text-gray-600">Über das Haus</a>
                 
 
             </div>
 
             <div className="flex-row space-x-5">
-                                <a href="#contact" className="hover:text-gray-600">Umgebung</a>
+                <a href="#umgebung" className="hover:text-gray-600">Umgebung</a>
 
-                <a href="#contact" className="hover:text-gray-600">Anreise</a>
-                <a href="#contact" className="hover:text-gray-600">Gästebuch</a>
+                <a href="#anreise" className="hover:text-gray-600">Anreise</a>
+                <a href="#gastebuch" className="hover:text-gray-600">Gästebuch</a>
 
-                <a href="#contact" className="hover:text-gray-600">Über Uns</a>
+                <a href="#uberUns" className="hover:text-gray-600">Über Uns</a>
                 
             </div>
             
@@ -35,7 +35,10 @@ export function Header()
         <div className=" absolute right-5 flex items-center space-x-6 text-black">
             <button className="btn">Hier Buchen!</button>
             <div className="size-10">
-                <FaUserCircle size={40}/>
+                <a className="inline-block transform transition-transform duration-700 hover:scale-120" href="#profile">
+                    <FaUserCircle size={40}/>
+                </a>
+                
 
             </div>
         </div>

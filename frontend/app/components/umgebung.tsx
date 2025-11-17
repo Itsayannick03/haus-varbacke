68c1797832a1b58bc3a9964524f3488724004bd1
Yannick Winkler <yannickwinkler3@gmail.com>
2025-11-17 15:11:37 +0100

    init umgebung

---
+++ frontend/app/components/umgebung.tsx
@@ -0,0 +1,12 @@
+
+
+export function Umgebung()
+{
+    return(
+        <div className="pt-25">
+            <div>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque eius error eligendi! Illo sit soluta voluptatibus amet quisquam voluptatum earum totam, aliquid, modi, temporibus nihil! Cupiditate repudiandae tempore aperiam.
+            </div>
+        </div>
+    )
+}

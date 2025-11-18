e4d71ae93c5f1fc05ef9b0caff72a631975d5782
Yannick Winkler <yannickwinkler3@gmail.com>
2025-11-18 15:40:48 +0100

    added destinationCard component

---
+++ frontend/app/components/destinationCard.tsx
@@ -0,0 +1,35 @@
+import "../styles/welcome.css"
+import "aos/dist/aos.css";
+import { LuMapPin } from "react-icons/lu";
+
+type DestinationCardProps = 
+{
+    image: string;
+    header: string;
+    description: string;
+    link: string;
+}
+
+export function DestinationCard({image, header, description, link}:DestinationCardProps)
+{
+    return(
+        <div className="p-6 gap-3 w-120 rounded-md border border-1px border-gray-200 shadow-lg flex justify-center items-center flex-col">
+                <div>
+                    <img
+                        src={image}
+                        alt="Source not found"
+                        className="float-left w-40 h-auto mr-4 mb-2 rounded-lg"
+                    />
+
+                    <h1 className="text-4xl font-serif">{header}</h1>
+
+                    <p className="text-[#4a4a4a] font-serif leading-relaxed">{description}</p>
+                </div>
+                
+                <a className="bg-[#eae6dc] border-2 border-[#cfc8b5] rounded-lg shadow-md w-full flex items-center justify-center" href={link}>
+                    <LuMapPin size={30} />
+                </a>
+
+            </div>
+    )
+}
--- frontend/app/components/umgebung.tsx
+++ frontend/app/components/umgebung.tsx
@@ -1,12 +1,50 @@
-
+import "../styles/welcome.css"
+import "aos/dist/aos.css";
+import { LuMapPin } from "react-icons/lu";
 
 export function Umgebung()
 {
     return(
-        <div className="pt-25">
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eaque eius error eligendi! Illo sit soluta voluptatibus amet quisquam voluptatum earum totam, aliquid, modi, temporibus nihil! Cupiditate repudiandae tempore aperiam.
+    <div className="pt-25 pb-5 bg-gradient-to-b from-[#f8f5ef] to-white flex flex-col items-center">
+           
+        
+            <div className="p-6 gap-3 w-120 rounded-md border border-1px border-gray-200 shadow-lg flex justify-center items-center flex-col">
+                <div>
+                    <img
+                        src="family.jpg"
+                        alt="description"
+                        className="float-left w-40 h-auto mr-4 mb-2 rounded-lg"
+                    />
+
+                    <h1 className="text-4xl font-serif">
+                        Rubrik
+                    </h1>
+
+                    <p className="text-[#4a4a4a] font-serif leading-relaxed">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus
+                        ligula vel ornare laoreet. Sed semper, sapien at venenatis malesuada,
+                        felis justo feugiat arcu, ut placerat lacus dolor id est. Vestibulum
+                        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+                        curae. Curabitur tincidunt eros nec nisl scelerisque, vitae vestibulum
+                        nunc maximus.
+                    </p>
+                </div>
+                
+
+                <button className="bg-[#eae6dc] border-2 border-[#cfc8b5] rounded-lg shadow-md w-full flex items-center justify-center">
+                    <LuMapPin size={30} />
+                </button>
+                
+
+            
             </div>
-        </div>
+           
+        
+       
+        
+        
+
+            
+    </div>
     )
 }

19d0ffd1aaf086ec845b07d8710bd99bfd4bbc9a
Yannick Winkler <yannickwinkler3@gmail.com>
2025-12-12 18:45:39 +0100

    modified pages

--- frontend/app/pages/guestbook.tsx
+++ frontend/app/pages/guestbook.tsx
@@ -12,13 +12,11 @@ export function meta({}: Route.MetaArgs) {
 export default function guestBook()
 {
   const users = [
-    {id: 1, name: "Yannick", age: 23},
-    {id: 2, name: "Alfred", age: 67},
-    {id: 3, name: "Alfred", age: 67},
-    {id: 3, name: "Alfred", age: 67},
-    {id: 3, name: "Alfred", age: 67},
-    {id: 3, name: "Alfred", age: 67},
-    {id: 3, name: "Alfred", age: 67},
+    {id: 1, fname: "Yannick", lname: "Winkler", date: "April 23 2021", comment: "Super duper truper!"},
+    {id: 1, fname: "Yannick", lname: "Winkler", date: "April 23 2021", comment: "Super duper truper!"},
+    {id: 1, fname: "Yannick", lname: "Winkler", date: "April 23 2021", comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At natus, numquam iusto beatae quia, odit quod a eum maxime quos illum? Quibusdam assumenda dignissimos officiis, perspiciatis perferendis molestias delectus nihil."},
+    {id: 1, fname: "Yannick", lname: "Winkler", date: "April 23 2021", comment: "Lorem ipsum dolor sit amet consectetur adipisicing elit. At natus, numquam iusto beatae quia, odit quod a eum maxime quos illum? Quibusdam assumenda dignissimos officiis, perspiciatis perferendis molestias delectus nihil."},
+    
   ];
     return(
       <div className="pt-40 pb-10 flex justify-center bg-gradient-to-b from-[#f8f5ef] to-white">
@@ -31,10 +29,10 @@ export default function guestBook()
 
           {users.map((user) => (
             <div className="page-content bg-[#fcf3f3]  color-[#4f2626] shadow-lg border-gray-200 gap-50px" key={user.id}>
-              <div className="flex overflow-hidden  flex-col items-center justify-center">
-                <h2>{user.name}</h2>
-                <p>Age: {user.age}</p>
-                <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. At natus, numquam iusto beatae quia, odit quod a eum maxime quos illum? Quibusdam assumenda dignissimos officiis, perspiciatis perferendis molestias delectus nihil.</p>
+              <div className="flex overflow-hidden font-serif  flex-col items-center justify-center">
+                <h2 className="text-3xl font-bold ">{user.fname} {user.lname}</h2>
+                <p> {user.date}</p>
+                <p className="">{user.comment}</p>
               </div>
               
             </div>
